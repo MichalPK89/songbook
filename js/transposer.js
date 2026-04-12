82f3d738781e22d2ea8502e78bf71a9838852738
MichalPK89 <miskomiskopk@gmail.com>
2026-04-12 20:46:45 +0200

    Update transposer.js

--- js/transposer.js
+++ js/transposer.js
@@ -1,4 +1,4 @@
-document.write('<button onclick="changeKey(-2)">-2</button>
+document.write(<button onclick="changeKey(-2)">-2</button>
   <button onclick="changeKey(-1)">-1</button>
   <button onclick="changeKey(1)">+1</button>
-  <button onclick="changeKey(2)">+2</button>')
+  <button onclick="changeKey(2)">+2</button>)
