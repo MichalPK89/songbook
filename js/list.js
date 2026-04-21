0f8953362a168fe885d4ced8c926735f709beb27
MichalPK89 <miskomiskopk@gmail.com>
2026-04-21 20:46:16 +0200

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,1 +1,16 @@
+const songs = {
+  Cervena_reka: {
+    title: "Červená řeka",
+    file: "Cervena_reka.html",
+    author: "tradicionál",
+    lyrics: "Ivo Fischer",
+    genre: "folk"
+  },
 
+  Other_song: {
+    title: "Name",
+    author: "autor",
+    genre: "rock",
+    file: "Other_song.html"
+  }
+}
