1296fa91dd4441f29c25f49635b54a0e536dc90a
MichalPK89 <miskomiskopk@gmail.com>
2026-04-27 08:30:52 +0200

    Update songbook.js

--- js/songbook.js
+++ js/songbook.js
@@ -227,11 +227,7 @@ function loadHTML(id, file) {
 <!-- renderuj text a akordy-->
 renderSong(song);
 
-<!-- vytvor zoznam piesní 
-function createSongList {
-	document.getElementById("list").innerHTML = "test1"
-}
--->
+
 
 
 
