1cbe453ed9b9778018abb6b0588a2e8e05919f9e
MichalPK89 <miskomiskopk@gmail.com>
2026-05-30 16:18:14 +0200

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -1,4 +1,4 @@
-fetch("json/list_moderne_test.json")
+fetch("https://michalpk89.github.io/songbook/json/list_moderne_test.json")
     .then(response => response.json())
     .then(songs => {
 		console.log(songs);
