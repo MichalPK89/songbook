939d7d9e46567fdc44a6ed7505cbef53aa6ddf97
MichalPK89 <miskomiskopk@gmail.com>
2026-06-14 20:22:42 +0200

    Update list_krestanske.js

--- js/list_krestanske.js
+++ js/list_krestanske.js
@@ -15,7 +15,7 @@ const songs = {
     video: "https://www.youtube.com/watch?v=505CrV4U664"
   },
 
-  hladnyk: {
+  hladny: {
     title: "Hladný (prichádzam)",
     performer: "Rieka života",
     music: "K. Scott",
