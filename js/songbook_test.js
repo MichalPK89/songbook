30d240d7110759a09977c8d25f488ffcf365d63b
MichalPK89 <miskomiskopk@gmail.com>
2026-06-18 16:52:07 +0200

    Update songbook_test.js

--- js/songbook_test.js
+++ js/songbook_test.js
@@ -11,7 +11,7 @@ async function initSongPage() {
 
     document.getElementById("info").innerText = song.title;
 
-	const songFile = "songs/" + songId + ".txt"
+	const songFile = folder + songId + ".txt"
 
     const lyrics = await fetch(songFile)
         .then(r => r.text());
