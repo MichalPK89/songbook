5b399402c169aeb955afd6a9a18fbe5405d960fe
MichalPK89 <miskomiskopk@gmail.com>
2026-06-21 19:40:58 +0200

    Update index_test.js

--- js/index_test.js
+++ js/index_test.js
@@ -38,7 +38,7 @@ async function downloadOffline() {
     // pridaj všetky txt
     Object.values(songs).forEach(song => {
 
-        files.push(song.lyricsFile);
+        files.push(song);
 
     });
 
