83f3c86c85fa4d6aa3fcacee019e1cb73b7309f4
MichalPK89 <miskomiskopk@gmail.com>
2026-06-23 20:08:40 +0200

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -2,6 +2,20 @@ const CACHE_NAME = "songbook-v1";
 
 self.addEventListener("fetch", event => {
 
+    const url = new URL(event.request.url);
+
+    // všetky songs.html?id=...
+    if (url.pathname.endsWith("songs.html")) {
+
+        event.respondWith(
+            caches.match("songs.html")
+                .then(response => response || fetch("songs.html"))
+        );
+
+        return;
+    }
+
+    // ostatné súbory
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
