8bd32b1397ef21169cc3c9a724366ab6b4b46f60
MichalPK89 <miskomiskopk@gmail.com>
2026-06-24 21:06:59 +0200

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,13 +1,63 @@
 const CACHE_NAME = "songbook-v3";
+
+const CACHE_NAME = "songbook-v3";
+
+self.addEventListener("fetch", event => {
+
+    console.log("========== FETCH ==========");
+    console.log("Request URL:", event.request.url);
+
+    const url = new URL(event.request.url);
+
+    event.respondWith(
+
+        caches.match(event.request, {
+            ignoreSearch: true
+        })
+
+        .then(response => {
+
+            console.log(
+                "Cache hit:",
+                response ? "YES" : "NO"
+            );
+
+            if (response) {
+
+                console.log("Serving from cache");
+
+                return response;
+            }
+
+            console.log("Fetching from network");
+
+            return fetch(event.request);
+
+        })
+
+        .catch(error => {
+
+            console.error("SW ERROR:", error);
+
+            throw error;
+
+        })
+
+    );
+
+});
+
 /*
 self.addEventListener("fetch", event => {
 
     if (!event || !event.request) return;
 
+    console.log("REQUEST:", event.request.url);
+
     const url = new URL(event.request.url);
     const cleanPath = url.pathname.split("?")[0];
 
-    console.log("FETCH:", event.request.url);
+
 
     // songs page fix
     if (cleanPath.endsWith("/songs.html")) {
@@ -24,22 +74,5 @@ self.addEventListener("fetch", event => {
         caches.match(event.request.url.split("?")[0])
             .then(res => res || fetch(event.request))
     );
-});
-*/
-self.addEventListener("fetch", event => {
+});*/
 
-    event.respondWith(
-        caches.match(event.request, {
-            ignoreSearch: true
-        }).then(response => {
-
-            if (response) {
-                return response;
-            }
-
-            return fetch(event.request);
-
-        })
-    );
-
-});
