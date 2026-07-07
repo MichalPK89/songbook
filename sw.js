2d9863682b36f053a67fe56a42ffd397c7429bd1
MichalPK89 <miskomiskopk@gmail.com>
2026-07-07 19:29:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,9 +21,7 @@ self.addEventListener("activate", event => {
 /* 3. FETCH (tvoja logika, zachovaná) */
 self.addEventListener("fetch", event => {
 
-    console.log("========== FETCH ==========");
-    console.log("Request URL:", event.request.url);
-
+   
     const url = new URL(event.request.url);
 
     event.respondWith(
@@ -34,8 +32,7 @@ self.addEventListener("fetch", event => {
 
         .then(response => {
 
-            console.log("Cache hit:", !!response);
-
+           
             if (response) {
                 return response;
             }
