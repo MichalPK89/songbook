c773a86e69b9347a27d2157374cd620a97004f40
MichalPK89 <miskomiskopk@gmail.com>
2026-07-15 15:26:40 +0200

    Update songbook.js

--- js/songbook.js
+++ js/songbook.js
@@ -173,7 +173,7 @@ function toggleChords() {
 function renderSettings() {
 
 	let transposeSign = (TRANSPOSE > 0) ? '+' : (TRANSPOSE == 0)? '&nbsp' : '';
-	let transposeShow = TRANSPOSE == 0)? '&nbsp' : '';
+	let transposeShow = (TRANSPOSE == 0)? '&nbsp' : '';
 
     document.getElementById("settings").innerHTML = `
         <p>
